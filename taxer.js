db579165ab908a494d4eeb4af630bdff80555fe9
Paul <pt2323@gmail.com>
2015-01-25 20:47:46 -0500

    add get child function

--- taxer.js
+++ taxer.js
@@ -5,7 +5,10 @@ var async = require('async');
 
 /**
 
-Options include: knexObject, tableName
+Options include:
+    knexObject,
+    tableName,
+    useCache
 
 */
 module.exports = function(options, callback){
@@ -16,8 +19,67 @@ module.exports = function(options, callback){
     }
 
     var self = this
-    this.knex = options.knex
-    this.tableName = options.tableName
+
+    self.knex = options.knex
+    self.tableName = options.tableName
+    self.useCache = (options.useCache) ? options.useCache : false;
+
+    if( self.useCache ){
+        self.parentChildMap = {};
+        self.childParentMap = {};
+    }
+
+    // adds id to hierarchy
+    // if parent is null or 0, id is at the top level
+    this.add = function(id, parent, callbackIn){
+
+        if( !parent ){ parent = 0 }
+
+        // add to DB
+        self.knex(self.tableName)
+            .insert({'child': id, 'parent': parent })
+            .then(function(){
+                if( self.useCache ){ self.updateMaps(parent, id); }
+                callbackIn()
+            })
+            .catch(callbackIn)
+    }
+
+    // gets childrent to depth, if depth is -1, will get all children
+    // passes back values as a single merged array
+    this.getChildren = function(id, depth, callbackIn){
+        if( self.useCache ){
+            this.getChildrenFromCache(id, depth, callbackIn);
+        } else {
+
+        }
+    }
+
+    // follows same spec as getChildren
+    this.getChildrenFromCache = function(id, depth, callbackIn){
+        var allChildren = {};
+        var children = self.parentChildMap[id.toString()];
+        var currentDepth = 0;
+
+        while( children.length !== 0 && (depth > currentDepth || depth < 0 ) ){
+            var nextChildren = [];
+            // for each child
+            _.each(children, function(topChild){
+                allChildren[topChild.toString()] = topChild;
+                // for each child of child
+                _.each(self.parentChildMap[topChild.toString()], function(child){
+                    if( (child !== id) &&
+                        (typeof(allChildren[child.toString()]) === 'undefined' )
+                    ){
+                        nextChildren.push(child)
+                    }
+                })
+            })
+            children = nextChildren.slice(0)
+            if( depth >= 0 ){ depth++ }
+        }
+        callbackIn(null, _.values(allChildren))
+    }
 
     this.init = function(){
         // check if table exists
@@ -26,14 +88,38 @@ module.exports = function(options, callback){
                 if( !exists ){
                     // create the table
                     self.knex.schema.createTable(self.tableName, function(table){
-                        table.integer('parent').index()
-                        table.integer('child').index()
+                        table.integer('parent')
+                        table.integer('child')
+                        table.primary(['parent', 'child'])
                     })
                     .then(function(){ callback(); })
                     .catch(callback)
                 } else { callback(); }
             })
             .catch(callback)
+    }
+
+    this.updateMaps = function(parent, child){
+
+        var parentString = parent.toString()
+        var childString = child.toString()
+
+        // initialize obj varialbes if unset
+        if( typeof(self.parentChildMap[parentString]) === 'undefined' ){
+            self.parentChildMap[parentString] = [];
+
+        }
+        if( typeof(self.childParentMap[childString]) === 'undefined' ){
+            self.childParentMap[childString] = [];
+        }
+
+        // add values to map if not already present
+        if( !_.contains(self.parentChildMap[parentString], child) ){
+            self.parentChildMap[parentString].push(child)
+        }
+        if( !_.contains(self.childParentMap[childString], parent) ){
+            self.childParentMap[childString].push(parent)
+        }
     }
 
     this.init();
--- test/test.js
+++ test/test.js
@@ -1,5 +1,8 @@
 (function(){
 
+	var async = require('async')
+	var assert = require('assert')
+	var _ = require('underscore')
 	var Taxer = require('../taxer')
 
 	var dbCreds = {
@@ -15,23 +18,72 @@
 
 	var knex = require('knex')(dbCreds)
 
-
 	var test = {};
+	var taxer;
 
-	test.runTest = function(){
-		var taxer = new Taxer({
+	test.runTest = function(callbackIn){
+		taxer = new Taxer({
 			knex: knex,
-			tableName: 'category'
+			tableName: 'test_category',
+			useCache: true
 		}, function(err){
-			if(err){ console.log(err); }
-			else{
-console.log('success');
+			if(err){ callbackIn(err); }
+			else{ test.runTests(callbackIn) }
+		})
+
+	}
+
+	test.runTests = function(callbackIn){
+
+		async.series([
+			// empty test table
+			function(callback){
+				knex('test_category').truncate()
+					.then(function(){ callback(); })
+					.catch(callback)
+			},
+
+			// add top level node
+			function(callback){ taxer.add(1, null, callback) },
+
+			// add child of top level node
+			function(callback){ taxer.add(2, 1, callback) },
+
+			// add second child
+			function(callback){ taxer.add(3, 1, callback) },
+
+			// add sub-child
+			function(callback){ taxer.add(4, 2, callback) },
+
+			function(callback){
+				taxer.getChildren(1, -1, function(err, children){
+					if(err){ callback(err); }
+					else{
+						assert((children.length === 3), 'Incorrect number of children returned.')
+						_.each([2, 3, 4], function(num){
+							assert(_.contains(children, num), 'Unexpected children returned')
+						})
+						console.log(children);
+					}
+
+				})
+			},
+
+		], callbackIn);
+	}
+
+	test.callTest = function(){
+		test.runTest(function(err){
+			if(err){
+				console.log( 'An error occurred: ')
+				console.log(err)
+			} else {
+				console.log('success')
 			}
 		})
 	}
 
-
-	test.runTest();
+	test.callTest();
 
 	module.exports = test
 
